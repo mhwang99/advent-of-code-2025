8cc5c93ee402e7c911bedcfcaa5659865a1fd9ab
Mark Hwang <mark.hwang@owl.co>
2025-12-03 22:20:07 -0500

    day 3

---
+++ src/aoc25/d03.clj
@@ -0,0 +1,35 @@
+(ns aoc25.d03
+  (:require [clojure.string :as s]
+            [aoc25.utils :refer :all]))
+
+
+(def in (mapv #(mapv atoi %) (get-line-chars)))
+
+(def get-max
+  (memoize
+    (fn [n l]
+      (if (= n 1)
+        (apply max l)
+        (let [l (vec l)
+              first-range (- (count l) (dec n))
+              mx (apply max (take first-range l))
+              ]
+          (->> (range first-range)
+               (filter #(= (nth l %) mx))
+               (mapv #(get-max (dec n) (drop (inc %) l)))
+               (apply max)
+               (+ (->> n dec (Math/pow 10) long (* mx)))))))))
+
+(defn q1 [bl]
+  (->> bl
+       (map #(get-max 2 %))
+       (apply +)))
+
+
+(defn q2 [bl]
+  (->> bl
+       (map #(get-max 12 %))
+       (apply +)))
+
+#_(q1 in)
+#_(q2 in)
